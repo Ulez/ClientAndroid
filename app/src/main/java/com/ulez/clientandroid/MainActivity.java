b9f51c6bfa48db047b37df298cfba84255bb9d6f
Ulez <1104128773@qq.com>
2019-07-11 17:15:45 +0800

    ...

--- app/src/main/java/com/ulez/clientandroid/MainActivity.java
+++ app/src/main/java/com/ulez/clientandroid/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -14,15 +15,17 @@ import java.io.InputStream;
 import java.net.Socket;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     public static final int MESSAGE_NEW_MSG = 1;
+    private static final int MESSAGE_STATUS_MSG = 2;
 
     private Socket socket;
     private MyHandler mHandler;
     private EditText etIp;
     private TextView tvMsg;
+    private TextView tvStatus;
     private String TAG = "MainActivity";
     private ListView listView;
     private ArrayList<String> adapterData;
@@ -35,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         etIp = findViewById(R.id.et_ip);
         tvMsg = findViewById(R.id.tv_msg);
+        tvStatus = findViewById(R.id.tv_status);
         listView = findViewById(R.id.lv);
 
 
@@ -44,8 +48,19 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(adapter);
 
         mHandler = new MyHandler(this);
+        findViewById(R.id.bt).setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bt:
+                connectToServer();
+                break;
+        }
+    }
 
+    private void connectToServer() {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -53,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                     String[] paras = etIp.getText().toString().split(":");
                     socket = new Socket(paras[0], Integer.parseInt(paras[1]));
                     InputStream inputStream = socket.getInputStream();
+                    mHandler.obtainMessage(MESSAGE_STATUS_MSG, "连接成功！").sendToTarget();
                     byte[] buffer = new byte[1024];
                     int len;
                     while ((len = inputStream.read(buffer)) != -1) {
@@ -60,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
                         mHandler.obtainMessage(MESSAGE_NEW_MSG, data).sendToTarget();
                     }
                 } catch (IOException e) {
+                    mHandler.obtainMessage(MESSAGE_STATUS_MSG, "连接失败！").sendToTarget();
                     e.printStackTrace();
                 }
             }
@@ -76,6 +93,9 @@ public class MainActivity extends AppCompatActivity {
             switch (msg.what) {
                 case MESSAGE_NEW_MSG:
                     activity.addNewMsg((String) msg.obj);
+                    break;
+                case MESSAGE_STATUS_MSG:
+                    activity.tvStatus.setText((String) msg.obj);
                     break;
             }
         }
